1cfed8c7838bbdaab9f9faeed42da0f6a0305d72
s.chepets <s.chepets@plarium.com>
2025-06-30 22:23:57 +0200

    Refactor `MainWindowView` and `MainWindowView.xaml`: simplify styles, remove unused drag functionality, and reorganize layout resources for improved maintainability.

--- ServerMultiTool/Views/Windows/MainWindowView.xaml.cs
+++ ServerMultiTool/Views/Windows/MainWindowView.xaml.cs
@@ -1,22 +1,14 @@
 ﻿using System.Windows;
-using System.Windows.Input;
 using ServerMultiTool.ViewModels.Windows;
 
 namespace ServerMultiTool.Views.Windows
 {
     public partial class MainWindowView : Window
     {
-
         public MainWindowView()
         {
             DataContext = new MainWindowViewModel();
             InitializeComponent();
-        }
-
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-                DragMove();
         }
     }
 }
