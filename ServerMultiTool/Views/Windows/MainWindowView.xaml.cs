6a0c87cc215daafdcab762816ecf55f7b00ca23c
s.chepets <s.chepets@plarium.com>
2025-07-10 21:33:38 +0200

    Add top bar with controls and improve drag functionality
    This update introduces a new top bar in `MainWindowView.xaml` that features a logo, title, and buttons for closing, restoring, and minimizing the window. The previous layout has been replaced to enhance the user interface with a more modern design.
    
    Additionally, the `DragArea_MouseLeftButtonDown` method in `MainWindowView.xaml.cs` has been updated to support double-click events for maximizing and restoring the window, while maintaining the ability to drag the window. A new variable, `_restoreForDragMove`, has been added to improve the drag operation experience.

--- ServerMultiTool/Views/Windows/MainWindowView.xaml.cs
+++ ServerMultiTool/Views/Windows/MainWindowView.xaml.cs
@@ -1,22 +1,21 @@
-﻿using System.Windows;
+﻿using ServerMultiTool.ViewModels.Windows;
+using System.Windows;
 using System.Windows.Input;
-using ServerMultiTool.ViewModels.Windows;
 
 namespace ServerMultiTool.Views.Windows
 {
     public partial class MainWindowView : Window
     {
+        private bool _restoreForDragMove = false;
+
         public MainWindowView()
         {
             DataContext = new MainWindowViewModel();
             InitializeComponent();
         }
 
-        private bool _restoreForDragMove = false;
-
         private void DragArea_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // Двойной клик — максимизация/восстановление
             if (e.ClickCount == 2)
             {
                 WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
@@ -26,7 +25,6 @@ namespace ServerMultiTool.Views.Windows
             if (WindowState == WindowState.Maximized)
             {
                 _restoreForDragMove = true;
-                // Захватываем мышь, чтобы отследить следующий MouseMove
                 Mouse.Capture((UIElement)sender);
             }
             else
@@ -49,7 +47,7 @@ namespace ServerMultiTool.Views.Windows
 
                 var screen = System.Windows.Forms.Screen.FromHandle(new System.Windows.Interop.WindowInteropHelper(this).Handle).WorkingArea;
                 Left = e.GetPosition(null).X - (ActualWidth * percentX);
-                Top = 2; // небольшой отступ от верхнего края
+                Top = 2;
 
                 DragMove();
             }
