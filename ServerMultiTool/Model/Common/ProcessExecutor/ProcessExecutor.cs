54af10d1215fe5c568fa760d50f6fe75968e35ad
s.chepets <s.chepets@plarium.com>
2025-07-25 19:54:36 +0200

    Refactor ProcessExecutor for improved error handling
    - Updated using directives for better organization.
    - Enhanced `StartProcessOnceAsync` with cancellation checks.
    - Modified `TryToSetWorkingDirectory` to validate directory existence.
    - Added null check for process in `RunProcessAndGetOutputAsync`.

--- ServerMultiTool/Model/Common/ProcessExecutor/ProcessExecutor.cs
+++ ServerMultiTool/Model/Common/ProcessExecutor/ProcessExecutor.cs
@@ -1,45 +1,49 @@
+using Microsoft.IdentityModel.Tokens;
+using ServerMultiTool.Model.Common.Logs;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.IdentityModel.Tokens;
-using ServerMultiTool.Model.Common.Logs;
 
 namespace ServerMultiTool.Model.Common.ProcessExecutor;
 
 public class ProcessExecutor(Logger logger)
 {
-    public async Task<ProcessOutput> StartProcessOnceAsync(ProcessStartInfo startInfo, CancellationToken cancellationToken = default) => 
-        await StartProcessWithRetriesAsync(startInfo,  1, cancellationToken);
+    public async Task<ProcessOutput> StartProcessOnceAsync(ProcessStartInfo startInfo, CancellationToken cancellationToken = default) =>
+        await StartProcessWithRetriesAsync(startInfo, 1, cancellationToken);
 
     public async Task<ProcessOutput> StartProcessWithRetriesAsync(ProcessStartInfo startInfo, int retryCount, CancellationToken cancellationToken = default)
     {
         var response = new ProcessOutput();
-        
+
         if (startInfo.WorkingDirectory.IsNullOrEmpty())
             TryToSetWorkingDirectory(startInfo);
-                
+
         startInfo.CreateNoWindow = true;
         startInfo.UseShellExecute = false;
         startInfo.RedirectStandardOutput = true;
         startInfo.RedirectStandardError = true;
 
         var baseMessage = $"Process {startInfo.FileName} {startInfo.Arguments}";
-        
+
         var startMessage = $"{baseMessage} has started. ";
         var successMessage = $"{baseMessage} has completed successfully. ";
         var errorMessage = $"{baseMessage} has failed. ";
-        
+
         var messageDetails = string.Empty;
 
         logger.LogInfoWithPublish(startMessage);
-        
+
         for (var retryNumber = 1; retryNumber <= retryCount; retryNumber++)
         {
             response = await RunProcessAndGetOutputAsync(startInfo, cancellationToken);
+
+            if (cancellationToken.IsCancellationRequested)
+                break;
+
             messageDetails = response.Output;
-            
+
             if (response.Success is not true)
             {
                 logger.LogWarnWithPublish(errorMessage + $"Retry {retryNumber} of {retryCount}.", messageDetails);
@@ -57,13 +61,13 @@ public class ProcessExecutor(Logger logger)
     private static void TryToSetWorkingDirectory(ProcessStartInfo startInfo)
     {
         var fileName = startInfo.FileName;
-        
-        if (!File.Exists(fileName)) 
+
+        if (!File.Exists(fileName))
             return;
-        
+
         var workingDirectory = Path.GetDirectoryName(fileName);
-        
-        if (Directory.Exists(workingDirectory)) 
+
+        if (Directory.Exists(workingDirectory))
             startInfo.WorkingDirectory = workingDirectory;
     }
 
@@ -73,12 +77,12 @@ public class ProcessExecutor(Logger logger)
         {
             using var process = new Process();
             process.StartInfo = startInfo;
-            
+
             if (process is null)
                 throw new InvalidOperationException("Failed to start process. Process is null");
 
             process.Start();
-        
+
             var outputTask = process.StandardOutput.ReadToEndAsync(cancellationToken);
             var errorTask = process.StandardError.ReadToEndAsync(cancellationToken);
 
