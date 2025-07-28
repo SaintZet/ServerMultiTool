4e6fa8bea6ad0865c84ed674467702a222bb9e1d
s.chepets <s.chepets@plarium.com>
2025-07-28 23:28:40 +0200

    #24 Add NumericValueConverter and enhance PipelineView layout
    Updated PipelineView.xaml to include NumericValueConverter for multiplying numeric values by a specified parameter. Replaced TextBlock with a Border and ScrollViewer for better message display. Disabled horizontal scrolling in ScrollingListBox elements across TabItems. Created NumericValueConverter.cs to implement IValueConverter for one-way data binding.

---
+++ ServerMultiTool/ViewModels/Converters/NumericValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ServerMultiTool.ViewModels.Converters
+{
+    public class NumericValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                double multiplier = 1.0;
+                if (parameter is string paramStr && double.TryParse(paramStr, out double paramValue))
+                {
+                    multiplier = paramValue;
+                }
+                else if (parameter is double paramDouble)
+                {
+                    multiplier = paramDouble;
+                }
+
+                return doubleValue * multiplier;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
